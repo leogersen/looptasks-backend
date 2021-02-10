c3aa3fbbe20124841c87f885ad0602aebff4f477
Leo Gersen <leogersen@outlook.com>
2021-02-09 23:49:03 -0300

    Validator

--- src/main/java/com/leogersen/looptasks/LoopTasksBackendApplication.java
+++ src/main/java/com/leogersen/looptasks/LoopTasksBackendApplication.java
@@ -3,8 +3,12 @@ package com.leogersen.looptasks;
 import com.leogersen.looptasks.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
@@ -20,4 +24,17 @@ public class LoopTasksBackendApplication implements RepositoryRestConfigurer {
 		config.exposeIdsFor(Task.class);
 	}
 
+	@Bean
+	public Validator validatior() {
+		return new LocalValidatorFactoryBean();
+	}
+
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener vrel) {
+		Validator validator = validatior();
+		vrel.addValidator("beforeCreate", validator);
+		vrel.addValidator("beforeSave", validator);
+
+
+	}
 }
